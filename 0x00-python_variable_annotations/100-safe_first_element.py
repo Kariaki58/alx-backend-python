fa311a834b4a6e86d3c1446c010c86a824451654
kariaki58 <programmerjavascript901@gmail.com>
2024-03-14 13:25:28 +0100

    fixing code

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Duck typing - first elment"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
