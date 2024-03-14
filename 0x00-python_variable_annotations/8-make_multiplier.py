7c2048e7597c35cdbb17b7f32aafe9bb45b3dede
kariaki58 <programmerjavascript901@gmail.com>
2024-03-14 12:47:11 +0100

    make multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """documentation 1"""
     def multiply(x: float) -> float:
+        """multiply function"""
         return multiplier * x
     return multiply
