f9a93218fca6130844b2ee55573be6409ed7d09a
kariaki58 <programmerjavascript901@gmail.com>
2024-03-18 22:46:54 +0100

    added documentation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""moudles"""
 import asyncio
 import random
 
 async def wait_random(max_delay: int = 10) -> float:
+    """wait function"""
     delay_data = random.uniform(0, max_delay)
     await asyncio.sleep(delay_data)
     return delay_data
