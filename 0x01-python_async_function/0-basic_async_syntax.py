731433d27f18d9893d51937569be042d6560bf0b
kariaki58 <programmerjavascript901@gmail.com>
2024-03-20 12:44:44 +0100

    anotate function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,6 +3,6 @@
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay :int = 10) -> float:
     """wait random function"""
     return random.uniform(0, max_delay)
