cdf78d5dd613a30d2a15bda51025026d3197f140
kariaki58 <programmerjavascript901@gmail.com>
2024-03-20 12:49:17 +0100

    fixing code

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """the basics of async"""
 import random
+import asyncio
 
 
 async def wait_random(max_delay :int = 10) -> float:
     """wait random function"""
-    return random.uniform(0, max_delay)
+    value =  random.uniform(0, max_delay)
+    await asyncio.sleep(value)
+    return value
