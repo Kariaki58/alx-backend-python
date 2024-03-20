eadc4138016c8923c6cf3526bd3b0efa1fc136c1
kariaki58 <programmerjavascript901@gmail.com>
2024-03-20 13:12:44 +0100

    added function documentation, fixed pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,8 +5,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait n - return a sorted list of random data"""
     list_content = []
-    
     for _ in range(n):
         value = await wait_random(max_delay)
         list_content.append(value)
