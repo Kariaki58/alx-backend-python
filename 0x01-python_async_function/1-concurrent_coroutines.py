1fe152c271cc03d24476895dddf4cbf6d9632a6b
kariaki58 <programmerjavascript901@gmail.com>
2024-03-20 13:07:42 +0100

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""lets execute multiple coroutines at the same time"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    list_content = []
+    
+    for _ in range(n):
+        value = await wait_random(max_delay)
+        list_content.append(value)
+    return sorted(list_content)
