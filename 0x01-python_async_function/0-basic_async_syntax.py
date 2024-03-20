56e6ccfd47e45935dda9f7ec192e78b3abcdc605
kariaki58 <programmerjavascript901@gmail.com>
2024-03-20 12:43:26 +0100

    added path

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,8 @@
-import asyncio
+#!/usr/bin/env python3
+"""the basics of async"""
 import random
 
 
 async def wait_random(max_delay=10):
+    """wait random function"""
     return random.uniform(0, max_delay)
